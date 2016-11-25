23a4d3cc6d7ccdb80e0cfac367055d1dd858de3d
ETU\a.gargasson <a.gargasson@rubika-edu.com>
2016-11-25 15:17:58 +0100

    ajout des variables de base

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour
+{
+    private Vector2 _direction;
+    private float _speed;
+    private int _playerNumber;
+
+    public enum role
+    {
+        Killer,
+        Sherif,
+        Innocent,
+    }
+
+    private role _role;
+
+    public Agent(Vector2 direction, float speed, int playerNumber, role theRole)
+    {
+        _direction = direction;
+        _speed = speed;
+        _playerNumber = playerNumber;
+        _role = theRole;
+    }
+}
