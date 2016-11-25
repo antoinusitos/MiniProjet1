85f9b27ed5cd708ee114466f3be51aeb485d67b4
ETU\a.gargasson <a.gargasson@rubika-edu.com>
2016-11-25 15:27:01 +0100

    ajout des constructeurs de base

--- Assets/Scripts/IA.cs
+++ Assets/Scripts/IA.cs
@@ -3,5 +3,8 @@ using System.Collections;
 
 public class IA : Agent 
 {
+    public IA(Vector2 direction, float speed, int playerNumber, role theRole) : base(direction, speed, playerNumber, theRole)
+    {
 
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,5 +3,8 @@ using System.Collections;
 
 public class Player : Agent
 {
-
+    public Player(Vector2 direction, float speed, int playerNumber, role theRole) : base(direction, speed, playerNumber, theRole)
+    { 
+        
+    }
 }
